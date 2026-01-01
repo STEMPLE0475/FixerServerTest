f74373a7687e449eaab67384b42008833eb99ba2
STEMULE <stemple0@naver.com>
2026-01-01 11:42:32 +0900

    Update RoomManager.h

--- RoomManager.h
+++ RoomManager.h
@@ -209,5 +209,5 @@ private:
     std::atomic<uint32_t> next_room_id_;
 
     boost::asio::steady_timer room_log_timer_;
-    std::chrono::minutes log_interval_{ 5 };
+    std::chrono::seconds log_interval_{ 120 };
 };
